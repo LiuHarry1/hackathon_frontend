379f22baecdcfc44c92008df857e53b766b73b91
茫茫人海一粒沙 <593954214@qq.com>
2023-09-22 23:06:59 +0800

    code change

--- src/pages/modules/Assignments.vue
+++ src/pages/modules/Assignments.vue
@@ -165,7 +165,7 @@ export default {
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement('a');
           link.href = url;
-          link.setAttribute('download', 'quiz_assignment.txt');
+          link.setAttribute('download', 'quiz_ai_generate.txt');
           document.body.appendChild(link);
           link.click();
         })
